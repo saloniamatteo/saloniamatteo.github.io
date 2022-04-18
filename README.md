f755e50d3383cdda79373e84a97e4574a813629c
saloniamatteo <saloniamatteo@pm.me>
2022-04-18 17:04:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# [saloniamatteo.top](https://saloniamatteo.top)
+Salonia Matteo's website hosted by GitHub pages.
---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	content: ['*.html', 'it/*.html', 'en/*.html', 'js/*.js'],
+	css: ['css/*.css'],
+	fontFace: true,
+	keyframes: true,
+	output: "css",
+	variables: true
+}
