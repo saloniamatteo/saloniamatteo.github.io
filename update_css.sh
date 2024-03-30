14f9abf84a819ef5870c691a7df365a1be53e690
saloniamatteo <saloniamatteo@pm.me>
2024-03-30 19:16:52 +0100

    Refactor landing pages & remove stale script

--- update_css.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Update BeerCSS (CSS + JS) based on version defined in CSS_VER
-
-# BeerCSS version
-CSS_VER="2.2.11"
-
-wget "https://cdn.jsdelivr.net/npm/beercss@${CSS_VER}/dist/cdn/beer.min.css" -O css/beer.min.css
-wget "https://cdn.jsdelivr.net/npm/beercss@${CSS_VER}/dist/cdn/beer.min.js"  -O js/beer.min.js
